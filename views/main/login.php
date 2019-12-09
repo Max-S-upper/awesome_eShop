1736102838bab2e8432621d7703f9fddfc41ce56
Maxim Sokolskyi <maxim.sokolsky@gmail.com>
2019-12-09 13:48:29 +0200

    Add log in page styles

--- views/main/login.php
+++ views/main/login.php
@@ -1,8 +1,9 @@
-<main>
+<main class="login">
+    <h1>Sign in:</h1>
     <form action="signed" method="post">
         <input type="email" name="email" placeholder="email" required>
         <input type="password" name="password" placeholder="password" required>
         <input type="submit" value="Sign in">
     </form>
-    <?php if ($err) echo $err; ?>
+    <p class="error"><?php if ($err) echo $err; ?></p>
 </main>
