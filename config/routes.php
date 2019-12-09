263b8816691c26597b8aaf8e243c7165234dc2ae
Maxim Sokolskyi <maxim.sokolsky@gmail.com>
2019-12-09 08:50:45 +0200

    Add authorization page, class to process it and the second header for authorized users

--- config/routes.php
+++ config/routes.php
@@ -2,4 +2,7 @@
 return array(
     '' => 'storage/get_products',
     'show/([0-9]+)' => 'storage/get_product_by_id/$1',
+    'login' => 'authorization/show_authorization_page',
+    'signed' => 'authorization/check_authorization',
+    'logout' => 'user/logout'
 );
---
+++ views/main/successful_authorization.php
@@ -0,0 +1,3 @@
+<main>
+    <h1 class="welcome-text">Welcome! You're logged in as <?= $usr_email; ?>.</h1>
+</main>
